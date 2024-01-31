e902b71304ef6d67c6c8e1b557b396145cb9cde0
ShumkovDA <dfre.griffin111@gmail.com>
2024-01-31 20:18:20 +0300

    #3 Был добавлен метод при нажатии кнопки входа

--- EnterWindow.xaml.cs
+++ EnterWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +23,36 @@ namespace Messsanger
     /// </summary>
     public partial class EnterWindow : Window
     {
+        static SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=Messanger; Integrated Security=SSPI;");
         public EnterWindow()
         {
             InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, RoutedEventArgs e)
+        {
+            conn.Open();
+            errorTextBlock.Text = "";
+            string email = "";
+            string password = "";
+            string query = @"select Email, Password from [User]";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = query;
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                email = reader[0].ToString();
+                password = reader[1].ToString();
+                if (email == loginBox.Text.Trim() && password == passwordBox.Password)
+                {
+                    SmsWindow smsWindow = new SmsWindow();
+                    smsWindow.Show();
+                    this.Close();
+                }
+            }
+            conn.Close();
         }
     }
 }
